96442765b192113b00b2760b1d19eb4cc5eaa6be
Breno Caldeira Nascimento <breno.cn@hotmail.com>
2022-12-03 21:30:43 -0300

    refactor part 1

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,22 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
-fn parse_input(filepath: &str) -> Vec<(String, String)> {
+fn parse_input(filepath: &str) -> Vec<String> {
     std::fs::read_to_string(filepath)
         .unwrap()
         .split("\n")
-        .map(|line| {
-            let half = line.len() / 2;
-            let (first, second) = line.split_at(half);
-
-            (first.to_string(), second.to_string())
-        })
-        .collect::<Vec<(String, String)>>()
+        .map(String::from)
+        .collect::<Vec<String>>()
 }
 
 fn repeats(first: &String, second: &String) -> Option<char> {
-    let mut char_count: HashMap<char, bool> = HashMap::new();
+    let mut chars = HashSet::new();
 
     for c in first.chars() {
-        char_count.insert(c.clone(), true);
+        chars.insert(c.clone());
     }
 
     for c in second.chars() {
-        if char_count.contains_key(&c) {
+        if chars.contains(&c) {
             return Some(c);
         }
     }
@@ -37,10 +32,14 @@ fn char_to_value(c: &char) -> i32 {
     }
 }
 
-fn solve(rucksacks: Vec<(String, String)>) -> i32 {
+fn solve(rucksacks: Vec<String>) -> i32 {
     rucksacks.iter()
-        .map(|(first, second)| {
-            char_to_value(&repeats(first, second).unwrap())
+        .map(|rucksack| {
+            let half = rucksack.len() / 2;
+            let (first, second) = rucksack.split_at(half);
+
+            let (first, second) = (first.to_string(), second.to_string());
+            char_to_value(&repeats(&first, &second).unwrap())
         })
         .sum()
 }
